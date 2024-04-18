a25b8867f1adb2af5068a308de348f5295dd11d5
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-04-18 11:11:22 -0400

    fix-web

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
 const mas = document.getElementById('lawn');
 const mass = document.getElementById('header');
 const masss = document.getElementById('frent');
 const mes = document.getElementById('titulo');
 const mess = document.getElementById('spa');
- 
 const homes = document.getElementById('home');
 const pie = document.getElementById('footer');
+ 
 
 document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
   if(scroll_position > 300 ) {
+     
     lawn.style.display = 'block';
     header.style.backgroundColor = '#0366b6';
     frent.style.width = '90%';
@@ -19,11 +21,8 @@ document.addEventListener('scroll', () => {
     frent.style.boxShadow = '0px 0px 5px rgb(22,56,87)';
     document.getElementById('lawn').innerHTML = 'Call-Now';
     homes.style.color = 'blue';
-    
      
-     
-   
-  } else{
+    } else{
     lawn.style.display = 'none';
     header.style.backgroundColor = '#04aa6d';
     frent.style.boxShadow = 'none';
@@ -42,23 +41,25 @@ document.addEventListener('scroll', () => {
     header.style.backgroundColor = '#299684';
     header.style.color = '#ffff';
     frent.style.display= 'none';
-    titulo.style.background = '#0366b6';
+   
     spa.style.color = '#fff';
     casas.style.backgroundColor = 'rgba(0,0,0,0.2)';
     footer.style.height = '120px';
     footer.style.backgroundColor = '#026484';
-     
-     
-
-    
-
   } else{
      frent.style.display= '';
-     titulo.style.background = 'none';
+     
         spa.style.color = '#000';
          casas.style.backgroundColor = '#fff';
          footer.style.height = '140px';
     footer.style.backgroundColor = '#299684';
-     
+    
   }
+});
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if(scroll_position > 1000){
+    cut.style.display='block';
+  }
+});
 });
